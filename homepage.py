5c28aeab9010d27a3896f049569c96c260472ce4
David-Bergin <db18115@students.sacredheart.school.nz>
2024-05-20 09:55:56 +1200

    Worked on some of the html for the homepage and started to put in CramBerry logo

---
+++ homepage.py
@@ -0,0 +1,9 @@
+from bottle import run, route, view, static_file, request
+
+
+
+@route('/static/<filepath:path>')
+def load_static(filepath):
+    return static_file(filepath, root='./static')
+
+    
